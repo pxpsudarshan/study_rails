e723245cc30d8cf2cf8e2fbd061f0fe328f45005
pxpsudarshan <78455353+pxpsudarshan@users.noreply.github.com>
2023-08-11 11:38:12 +0900

    change

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,7 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link kaisha/company_stores.js
+//= link kaisha/offers.js
+//= link kaisha/job_profiles.js
+//= link kaisha/comps.js
