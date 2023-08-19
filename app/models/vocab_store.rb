9658c144858c3fa5dc4399e2ac254e469d2733c2
softverseAdmin <softverse4@gmail.com>
2023-08-19 15:54:03 +0900

    add quiz button  and quiz function
    add quiz button  and quiz function

--- app/models/vocab_store.rb
+++ app/models/vocab_store.rb
@@ -1,4 +1,5 @@
 class VocabStore < ApplicationRecord
   acts_as_paranoid
   records_with_operator_on :create, :update, :destroy
+  has_many :vocab_mycards, foreign_key: 'vocab_org', primary_key: 'vocab_org'
 end
