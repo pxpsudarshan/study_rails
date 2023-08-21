2df70f0b76d5e7114ea18d2388fdd2cf36135fbc
softverseAdmin <softverse4@gmail.com>
2023-08-21 18:46:01 +0900

    #mycard ko data create garda kheriko error solved

--- app/models/vocab_mycard.rb
+++ app/models/vocab_mycard.rb
@@ -2,5 +2,5 @@ class VocabMycard < ApplicationRecord
   acts_as_paranoid
   records_with_operator_on :create, :update, :destroy
   belongs_to :user
-  belongs_to :vocab_stores, foreign_key: 'vocab_org', primary_key: 'vocab_org'
+  belongs_to :vocab_store, foreign_key: :vocab_org, primary_key: :vocab_org, class_name: 'VocabStore'
 end
