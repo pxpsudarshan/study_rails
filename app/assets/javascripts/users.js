1861c20a5534f37dbb344b9201b0bdd053adf6f5
pxpsudarshan <78455353+pxpsudarshan@users.noreply.github.com>
2023-10-14 21:32:00 +0900

    修正更新

--- app/assets/javascripts/users.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    hsize = $(window).height();
-    $("my-image").css("height", hsize + "px");
-});
-
-$(window).resize(function () {
-    hsize = $(window).height();
-    $("my-image").css("height", hsize + "px");
-});
