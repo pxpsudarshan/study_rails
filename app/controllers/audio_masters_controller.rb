ec4a936620a05eee90deb45c2d889d38e8660c7a
pxpsudarshan <78455353+pxpsudarshan@users.noreply.github.com>
2024-11-21 08:36:52 +0900

    tab_added

--- app/controllers/audio_masters_controller.rb
+++ app/controllers/audio_masters_controller.rb
@@ -1,4 +1,11 @@
 class AudioMastersController < ApplicationController
     def index
+        puts "hello"
+        @count = 0
+        @audio_a = []
+        @audio_a = AudioA.order(:sort)
+        if params[:check_id].present?
+            @count = 1
+        end
     end
 end
